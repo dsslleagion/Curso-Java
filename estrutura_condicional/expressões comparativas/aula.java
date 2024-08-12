706456c5cfe46030b02e3728af8b382906ecbe00
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-08-12 07:49:30 -0300

    12_08

---
+++ estrutura_condicional/expressões comparativas/aula.java
@@ -0,0 +1,16 @@
+//Operadores comparativos 
+// >  maior
+// < menor 
+//  >= maior ou igual
+// <= menor ou igual
+// == igual
+// != diferente 
+
+public class aula{
+
+    public static void main(String[] args) {
+     
+
+    }
+
+}
