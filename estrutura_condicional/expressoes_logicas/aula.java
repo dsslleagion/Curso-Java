95213796c03b0b378ee74e83919e17ad98b1aae3
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-08-12 13:13:54 -0300

    feature: 12_08_24

---
+++ estrutura_condicional/expressoes_logicas/aula.java
@@ -0,0 +1,36 @@
+// operadores logicos 
+//&& E
+// || OU
+// ! NÃO
+
+
+// && todos os argumentos devem ser verdadeiros para retornar verdadeiros
+//ex:
+//int x = 5
+// x <= 20 && x == 10                     : F
+// x > 0 && x != 3                        : V
+// x <= 20 && x == 10 && X != 3           :F
+
+
+
+// || todos os argumentos devem possuir pelo menos 1 condição dee ser verdadeira 
+// int x = 5;
+// x== 10 || x<=20                        :V
+//x > 0 || x !=3                          :V
+//x <= 0 || x !=3 || x != 5               :V
+
+// ! inverte condição dos argumentos 
+// int x = 5;
+// !(x == 10)
+// !(X >= 2)
+// !(x <= 20 && x == 10)
+//verdadeiro é falso
+//falso é verdadeiro
+package estrutura_condicional.expressoes_logicas;
+
+public class aula {
+  public static void main(String[] args) {
+   
+    
+}
+}
