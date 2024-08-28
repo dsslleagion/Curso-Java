5bb1446c424425d8e48f52875f477f4dfcb52966
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-08-28 19:42:01 -0300

    28_08

---
+++ estruturas repetitivas/teste de mesa/exec3.java
@@ -0,0 +1,9 @@
+public class exec3 {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 100;
+        while(x != y){
+            System.out.println("olha");
+        }
+    }
+}
