8ab8e6451d4453774ecd01cb9d3629cd4ecc02be
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-09-20 10:13:14 -0300

    20_09

---
+++ introdução a programação Orientada a Objetos/Começando a resolver um seg problema exemplo/Program.java
@@ -0,0 +1,39 @@
+/*
+ * Fazer um programa para ver os dados de um produto 
+ * em estoque(nome,preco e quantidade de no estoque). em seguida:
+ * 
+ * - Mostrar os dados do produto (nome, preco, quantidade no estoque,valor
+ * total no estoque)
+ * - Realizar uma entrada no estoque e mostrar novamente os dados do produto
+ * - Realizar Uma saide no estoque e mostrar novamente os dados do produto
+ * Uma Classe .
+ * 
+*/
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Product;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        
+        System.out.println("Enter product data: ");
+        System.out.print("Name:  ");
+        product.name = sc.nextLine();
+
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+
+        
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println(product.name+ " , " + product.price + " , " + product.quantity);
+        sc.close();
+    }
+}
