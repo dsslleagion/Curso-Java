2d85dcf7ecb55643ed5d87d387fe50df16657841
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-09-20 17:30:50 -0300

    20_09

---
+++ introdução a programação Orientada a Objetos/Object e toString/aula.java
@@ -0,0 +1,10 @@
+/*
+ * -Toda classe em java é uma subclasse de classe Object
+ * 
+ * -Object possui os seguintes métodos:
+ * 
+ * -getClass - retorna o tipo do objeto
+ * - equals - compara se o objeto é igual ao outro 
+ * hashCode - retirna um codigo hash do objeto
+ * toString - converte o bjeto para String
+ */
