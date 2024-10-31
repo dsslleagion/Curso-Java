1605ec95ab913b89f5f2e405307e680ed29f4fef
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-10-31 17:06:24 -0300

    31_10

---
+++ Construtores, palavra this, sobrecarga, encapsulamento/palavra this/entities/Product.java
@@ -0,0 +1,42 @@
+package entities;
+
+public class Product {
+
+public String name;
+public double price;
+public int quantity;
+
+// adiciona o construtor em uma classe
+// quando deseja obrigar que os valores da classe
+// não sejam vazios
+
+//o construtor só executa no momento 
+//da instanciaçã odo objeto
+public Product(String name, double price, int quantity){
+    //"this" referencia do proprio objeto
+    //primeiro variavel "name" referencia ao atributo da classe,
+    // e o segundo name é referencia ao parametro da função construtora
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+}
+
+public double totalValueInStock() {
+return price * quantity;
+}
+public void addProducts(int quantity) {
+this.quantity += quantity;
+}
+public void removeProducts(int quantity) {
+this.quantity -= quantity;
+}
+public String toString() {
+return name
++ ", $ "
++ String.format("%.2f", price)
++ ", "
++ quantity
++ " units, Total: $ "
++ String.format("%.2f", totalValueInStock());
+}
+}
